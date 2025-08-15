0a83e38a25709f77b092da783b627e0f3992b1b7
vedasree <anapved8@gmail.com>
2025-08-15 17:21:06 +0530

    Modifications to SportsArena

--- src/main/java/com/example/Sports_Arena/Controller/SportsArenaController.java
+++ src/main/java/com/example/Sports_Arena/Controller/SportsArenaController.java
@@ -1,14 +1,18 @@
 package com.example.Sports_Arena.Controller;
 
 
+import com.example.Sports_Arena.Model.LocationDTO;
 import com.example.Sports_Arena.Model.SportsArena;
 import com.example.Sports_Arena.Model.SportsArenaWithCourts;
 import com.example.Sports_Arena.Service.SportsArenaService;
+import com.example.Sports_Arena.Service.UserLocation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/arena")
@@ -16,6 +20,9 @@ public class SportsArenaController {
 
     @Autowired
     SportsArenaService sportsArenaService;
+
+    @Autowired
+    UserLocation userLocation;
 //fetch based on nearest location
     @GetMapping("/getAll")
     public List<SportsArena> getAllSportsArena(){
@@ -34,6 +41,14 @@ public class SportsArenaController {
     @GetMapping("/{arenaId}/courts")
     public SportsArenaWithCourts getAllCourtdForArena(@PathVariable Long arenaId){
         return sportsArenaService.getAllCourtsForArena(arenaId);
+    }
+    @GetMapping("/users/{id}/location")
+    public List<SportsArena> getNearestSportsArena(@PathVariable long id) {
+        LocationDTO locationDTO= userLocation.getUserLocation(id).getBody();
+        double userLat = locationDTO.getLatitude();
+        double userLng = locationDTO.getLongitude();
+        //List<SportsArena>
+        return new ArrayList<>();
     }
 
 
